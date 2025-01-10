0b1772c79f2b510a513a90b6891ee9f348da9805
Laura Margherita <laura@mintflavour.com>
2025-01-10 08:57:05 +0100

    base project

--- main.js
+++ main.js
@@ -1,27 +1,53 @@
-import * as THREE from 'three';
+import * as THREE from "three";
+import getLayer from "./libs/getLayer.js";
+import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader.js";
+import getStarfield from "./libs/getStarfield.js";
+
+const w = window.innerWidth;
+const h = window.innerHeight;
 
 const scene = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 1000);
+camera.position.z = 5;
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+const canvas = document.getElementById('three-canvas');
+
+const renderer = new THREE.WebGLRenderer({ antialias: true, canvas });
+renderer.setSize(w, h);
 
 const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0xff000 });
+const material = new THREE.MeshStandardMaterial({ color: 0xffff00 });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
-camera.position.z = 5;
+const hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444);
+scene.add(hemiLight);
 
-const animate = function () {
+const  gradientLayer = getLayer({
+    hue: 0.6,
+    numSprites: 8,
+    opacity: 0.2,
+    radius: 10,
+    size: 24,
+    z: -10.5,
+})
+scene.add(gradientLayer);
+
+function animate() {
     requestAnimationFrame(animate);
-    
     cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-    
+    cube.rotation.y += 0.02;
     renderer.render(scene, camera);
-};
-
+}
 animate();
+
+window.addEventListener("scroll", () => {
+})
+
+function handleResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
+window.addEventListener('resize', handleResize, false);
